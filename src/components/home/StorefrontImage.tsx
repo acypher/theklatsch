7cba4ea0edd717590d4490215a96d361e7e960ba
Allen Cypher <allencypher@gmail.com>
2025-10-11 10:31:01 -0700

    Update StorefrontImage.tsx
    back from tarplant to the usual

--- src/components/home/StorefrontImage.tsx
+++ src/components/home/StorefrontImage.tsx
@@ -3,7 +3,7 @@ export const StorefrontImage = () => (
   <div className="mt-16 mb-8 flex flex-col items-center">
     {/* Previous image was: "/lovable-uploads/17100c7f-adac-4287-bf4c-d08288a0c3f5.png" */}
     <img 
-      src="https://saveplants.org/wp-content/uploads/2022/09/Santa-Cruz-tarplant.jpg" 
+      src="/lovable-uploads/17100c7f-adac-4287-bf4c-d08288a0c3f5.png" 
       alt="The Klatsch Storefront" 
       className="w-1/4 h-auto object-cover rounded-lg shadow-md max-w-[240px]"
     />
