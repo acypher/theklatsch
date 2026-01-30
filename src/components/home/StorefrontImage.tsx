5f3666f5cf93957d5a14cf4f167ed5cf34b8f4b8
Allen Cypher <allencypher@gmail.com>
2026-01-30 00:21:50 -0800

    Update storefront image to February Image of the Month

--- src/components/home/StorefrontImage.tsx
+++ src/components/home/StorefrontImage.tsx
@@ -1,10 +1,9 @@
-
 export const StorefrontImage = () => (
   <div className="mt-16 mb-8 flex flex-col items-center">
     {/* Previous image was: "/lovable-uploads/17100c7f-adac-4287-bf4c-d08288a0c3f5.png" */}
-    <img 
-      src="/lovable-uploads/17100c7f-adac-4287-bf4c-d08288a0c3f5.png" 
-      alt="The Klatsch Storefront" 
+    <img
+      src="https://kjfwyaniengzduyeeufq.supabase.co/storage/v1/object/public/article-images/1769760517953-woqxqddgfsf.jpeg"
+      alt="February Image of the Month"
       className="w-1/4 h-auto object-cover rounded-lg shadow-md max-w-[240px]"
     />
   </div>
